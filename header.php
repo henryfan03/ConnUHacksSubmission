54fdfc8b16a1214eb0f97e924d1e2ac0c0ecf11a
lpoon <lucaslfpoon@gmail.com>
2022-01-29 13:24:24 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>website</title>
+  </head>
+  <body>
+    <div class="header">
+      <div class="left">
+        <a href="#"><img src="images\SAP.png" style="height: 70px;"></a>
+      </div>
+      <div class="right">
+        <a href="#">Orders</a>
+        <a href="#">Login</a>
+      </div>
+    </div>
