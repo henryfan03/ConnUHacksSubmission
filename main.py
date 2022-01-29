450a0227a0f42ee938c484e8859c04184b3ec2c5
henryfan2313 <68411149+henryfan03@users.noreply.github.com>
2022-01-29 18:34:47 +0000

    added getOrderByID and modified getOrderByEmail

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import json
 
 def main():
   getCustomerOrder()
-  getOrderById('abbigail.kautzer.764458@test.com')
+  #getOrderById('abbigail.kautzer.764458@test.com')
 
 def getCustomerOrder():
   response = requests.get('https://sapstore.conuhacks.io/orders/')
   with open('orderData.json', 'w') as orderData:
     json.dump(response.json(), orderData)
 
-def getOrderById(id):
-  URL = 'https://sapstore.conuhacks.io/orders/byEmail?email=' + id
-  print(URL)
+def getOrderByEmail(email):
+  URL = 'https://sapstore.conuhacks.io/orders/byEmail?email=' + email
   response = requests.get(URL)
-  print(response)
+  with open('orderCache.json', 'w') as orderData:
+    json.dump(response.json(), orderData)
+
+def getOrderByID(id):
+  URL = 'https://sapstore.conuhacks.io/orders/' + id
+  response = requests.get(URL)
   with open('orderCache.json', 'w') as orderData:
     json.dump(response.json(), orderData)
 
