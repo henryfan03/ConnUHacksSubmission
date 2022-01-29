25bff7d1b12868683bc69fc765c59b809b34637c
henryfan2313 <68411149+henryfan03@users.noreply.github.com>
2022-01-29 21:38:38 +0000

    fixed an error with json file interpreter

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 
 def main():
   getCustomerOrder()
-  getFromJSON("orderData.json")
-  #getOrderById('abbigail.kautzer.764458@test.com')
+  getOrderByEmail('abbigail.kautzer.764458@test.com')
+  localdata = getFromJSON("orderCache.json")
+  print(localdata['orderId'])
 
 def getCustomerOrder():
   response = requests.get('https://sapstore.conuhacks.io/orders/')
@@ -15,29 +16,29 @@ def getOrderByEmail(email):
   URL = 'https://sapstore.conuhacks.io/orders/byEmail?email=' + email
   response = requests.get(URL)
   with open('orderCache.json', 'w') as orderData:
-    json.dump(response.json(), orderData)
+    json.dump(response.json()[0], orderData)
 
-def getOrderByID(id):
+def getOrderById(id):
   URL = 'https://sapstore.conuhacks.io/orders/' + id
   response = requests.get(URL)
   with open('orderCache.json', 'w') as orderData:
-    json.dump(response.json(), orderData)
+    json.dump(response.json()[0], orderData)
 
 def getStores():
   response = requests.get('https://sapstore.conuhacks.io/stores')
   with open('storeData.json', 'w') as storeData:
     json.dump(response.json(), storeData)
 
-def getStoreByID(id):
+def getStoreById(id):
   URL = 'https://sapstore.conuhacks.io/stores/' + id
   response = requests.get(URL)
   with open('storeCache.json', 'w') as storeData:
-    json.dump(response.json(), storeData)
-
+    json.dump(response.json()[0], storeData)
+  
 def getFromJSON(filename):
   with open(filename) as filedata:
     data = json.load(filedata)
-    print(data)
+  return data
 
 if __name__ == "__main__":
   main()
