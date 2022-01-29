335b298c1adc7abf40f8ee20c5153ac2924ca52e
lpoon <lucaslfpoon@gmail.com>
2022-01-29 14:17:18 -0500

    d

--- header.php
+++ header.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>website</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.10.4/brython.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.10.4/brython_stdlib.min.js"></script>
   </head>
-  <body>
+  <body onload="brython()">
+    <script type"text/python" src="main.py"></script>
     <div class="header">
       <div class="left">
         <a href="#"><img src="images\SAP.png" style="height: 70px;"></a>
